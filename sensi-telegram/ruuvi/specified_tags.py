6f463039cee7fc626ed3d8b652dedd8b3ca0a2af
TALLES VALIATTI <talles.dsv@gmail.com>
2018-08-11 09:01:26 -0700

    8th cmmit

--- sensi-telegram/ruuvi/specified_tags.py
+++ sensi-telegram/ruuvi/specified_tags.py
@@ -6,28 +6,30 @@ import time
 
 # List of macs of sensors which data will be collected
 # If list is empty, data will be collected for all found sensors
-macs = ['D7:05:12:28:73:D9',]
+macs = ['D7:05:12:28:73:D9', 'F7:82:69:C1:8A:F9' ]
 # get_data_for_sensors will look data for the duration of timeout_in_sec
 timeout_in_sec = 10
 i=0
 while 1:
     i=i+1
     print(i)
-    try:
-        datas = RuuviTagSensor.get_data_for_sensors(macs, timeout_in_sec)
-        # Dictionary will have lates data for each sensor
-        tags = datas['D7:05:12:28:73:D9']
-        #print(datas['D7:05:12:28:73:D9'])
-        for var in tags:
-            print(var +" : " +str( tags[var]) )
-        date = datetime.datetime.now()
-        print("Ano : " + str(date.year))
-        print("Mês : " + str(date.month))
-        print("dia : " + str(date.day))
-        print("hora : " + str(date.hour))
-        print("minuto : " + str(date.minute))
-        print("segundo : " + str(date.second))
-        print("\n\n--------------------------\n\n")
-    except KeyError:
-        print ('continua')
+    datas = RuuviTagSensor.get_data_for_sensors(macs, timeout_in_sec)
+    # Dictionary will have lates data for each sensor
+    for mac in macs:
+        try:
+            tags = datas[mac]
+            # print(datas['D7:05:12:28:73:D9'])
+            for var in tags:
+                print(var + " : " + str(tags[var]))
+            date = datetime.datetime.now()
+            print("Ano : " + str(date.year))
+            print("Mês : " + str(date.month))
+            print("dia : " + str(date.day))
+            print("hora : " + str(date.hour))
+            print("minuto : " + str(date.minute))
+            print("segundo : " + str(date.second))
+            print("\n\n--------------------------\n\n")
+        except KeyError:
+            print ('continua')
+
     time.sleep(5)
