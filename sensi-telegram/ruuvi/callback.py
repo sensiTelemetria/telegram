4c250a0c0f7bce1aa2396e7fe48eec91f7b31502
TALLES VALIATTI <talles.dsv@gmail.com>
2018-08-11 08:34:13 -0700

    7th cmmit

---
+++ sensi-telegram/ruuvi/callback.py
@@ -0,0 +1,7 @@
+from ruuvitag_sensor.ruuvi import RuuviTagSensor
+
+def handle_data(found_data):
+    print('MAC ' + found_data[0])
+    print(found_data[1])
+
+RuuviTagSensor.get_datas(handle_data)
