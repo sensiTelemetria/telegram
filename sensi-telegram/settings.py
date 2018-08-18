e551ba2869ff16a1beccb3216a92e4c64fbe0154
TALLES VALIATTI <talles.dsv@gmail.com>
2018-08-18 08:06:48 -0700

    matplotlib 11

--- sensi-telegram/settings.py
+++ sensi-telegram/settings.py
@@ -4,6 +4,7 @@ dataBaseDjangoDir = '/home/pi/Desktop/Django/SensiProject/db.sqlite3'
 #path do banco de dados das tags
 dataBaseSensiDir = '/home/pi/Desktop/sensi.db'
 
+#path dos arquivos temporários
 tempDir = '/home/pi/Desktop/'
 
 #dados sobre o cliente/
@@ -14,3 +15,6 @@ local = 'HUCAM'
 nameCompany = 'sensi telemetria'
 owners = {1:['Willian ferreira', '(27) 9999-9999'],2:['Talles D. de S. Valiatti','(27) 9999-9999']}
 site = 'www.sensitelemetria.com'
+
+#django
+siteConfig = "192.168.1.2:8000"
